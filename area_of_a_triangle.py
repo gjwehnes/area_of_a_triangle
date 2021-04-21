1b0ecd8dc8417785dd4f74908e6718e7f83fdc96
gjwehnes <gjwehnes@shaw.ca>
2021-04-20 19:53:04 -0600

    Update area_of_a_triangle.py

--- area_of_a_triangle.py
+++ area_of_a_triangle.py
@@ -1,6 +1,1 @@
-#base = float(input("What is the base of the triangle?:"))
 
-#height = float(input("What is the height of the triangle?:"))
-#area = base * height / 2
-
-#print("The area is " + str(area))
